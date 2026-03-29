818d70ad39494e2a633a6defc413e11f952b4edb
luya <91703950+lmaqungo@users.noreply.github.com>
2026-03-29 16:14:32 +0200

    fix: updated cookie config

--- src/config/sessionStore.ts
+++ src/config/sessionStore.ts
@@ -1,10 +1,16 @@
 import { prisma } from "../lib/prisma.js";
 import { PrismaSessionStore } from "@quixo3/prisma-session-store";
 import expressSession from 'express-session'
+import "dotenv/config";
+
+const { NODE_ENV } = process.env
+
 
 const session =  expressSession({
     cookie: {
-        maxAge: 7 * 24 * 60 * 60 * 1000
+        maxAge: 7 * 24 * 60 * 60 * 1000, 
+        secure: true, 
+        sameSite: NODE_ENV === 'prod' ?  'none' : false
     }, 
     secret: 'cats', 
     resave: false, 
